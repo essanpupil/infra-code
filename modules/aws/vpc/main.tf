75505ce44b119fad27a6f8a587150ef3bd49d431
Ikhsan N. Rosyidin <essanpupil@gmail.com>
2026-09-25 13:53:02 +0700

    refacotr modules

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -101,7 +101,7 @@ resource "aws_route" "private_default" {
 
   route_table_id         = aws_route_table.private[each.key].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id          = aws_nat_gateway.this[local.private_nat_index[each.key]].id
+  nat_gateway_id         = aws_nat_gateway.this[local.private_nat_index[each.key]].id
 }
 
 resource "aws_route_table_association" "private" {
